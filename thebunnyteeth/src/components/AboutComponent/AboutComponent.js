3e14b2b1f38cede5005c8340e72ecca179c4d5c6
sugumar <sugumar@bitcot.com>
2022-06-20 10:09:05 +0530

    about screen adding

--- thebunnyteeth/src/components/AboutComponent/AboutComponent.js
+++ thebunnyteeth/src/components/AboutComponent/AboutComponent.js
@@ -151,26 +151,7 @@ export default (props) => {
                     </div>
                 </div>
             </section>
-            <section className="our_clinic maintain_div py_80">
-                <div className="our_clinic_wrap maintain_div py_80 pt-3">
-                    <div className="container custom_container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <header className="maintain_div mb_30 text-center">
-                                    <h3 className="h3_title montserrat_font">Our Clinic Interior</h3>
-                                </header>
-                                <div className="oc_card maintain_div">
-                                    <div className="video_data">
-                                        <iframe width="100%" height="630" src="https://www.youtube.com/embed/8Xv0K2RJ8eo?feature=oembed&rel=0&mute=0&loop=0&controls=1">
-                                        </iframe>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="our_specialty maintain_div">
+            <section className="our_specialty maintain_div py_80">
                 <div className="our_specialty_wrap maintain_div py_80 pt-3">
                     <div class="card-background-overlay"></div>
                     <div className="container custom_container">
@@ -258,8 +239,184 @@ export default (props) => {
                     </div>
                 </div>
             </section>
+            <section className="our_clinic maintain_div">
+                <div className="our_clinic_wrap maintain_div py_80 pt-3">
+                    <div className="container custom_container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <header className="maintain_div mb_30 text-center">
+                                    <h3 className="h3_title montserrat_font">Our Clinic Interior</h3>
+                                </header>
+                                <div className="oc_card maintain_div">
+                                    <div className="video_data">
+                                        <iframe width="100%" height="630" src="https://www.youtube.com/embed/8Xv0K2RJ8eo?feature=oembed&rel=0&mute=0&loop=0&controls=1">
+                                        </iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="our_specialty maintain_div">
+                <div className="our_specialty_wrap maintain_div py_80 pt-3">
+                    <div class="card-background-overlay"></div>
+                    <div className="container custom_container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="row align-items-center">
+                                    <div className="col-lg-4 d-flex">
+                                        <div className="left_content maintain_div">
+                                            <article className="main_article maintain_div">
+                                                <h4 className="h4_title text-left text_color montserrat_font margin_B20 secondaryColor">WHY TRUST</h4>
+                                                <h3 className="ds_info text-left text_color montserrat_font">THE BUNNY TEETH? </h3>
+                                                <progress id="file" value="25" max="100" style={{ width: "75%", height: "3px" }}>25%</progress>
+                                                <p className="oc_info text-left text_color roboto_font">
+                                                    We contend to provide a comprehensive oral healthcare solution with sophisticated yet affordable state-of-the art infrastructure. Collaborating prominent doctors and cutting edge technology, we are determined to provide exceptional service to anyone and everyone
+                                                </p>
+                                            </article>
+                                            <div className="theme_image">
+                                                <img src={baseImagePath('icons/theme1.png')} class="custom_img"
+                                                    alt="Alphabet Internation School" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 ">
+                                        <div className="card_divider maintain_div mt-4">
+                                            <article className="main_article maintain_div text-center">
+
+                                                <div className="mb-3 maintain_div">
+                                                    <h4 className="h4_title text-center text_color montserrat_font" style={{ lineHeight: "40px" }}>REVIEW MECHANISM
+                                                    </h4>
+                                                </div>
+                                                <p className="card_info text-center m-0 text_color roboto_font">
+                                                    Our close call on each and every patient, before and after their treatment, gives them immense confidence that they are being taken care in the right way
+                                                </p>
+                                            </article>
+                                        </div>
+                                        <div className="card_divider maintain_div mt-4">
+                                            <article className="main_article maintain_div text-center">
+                                                <div className="mb-3 maintain_div">
+                                                    <h4 className="h4_title text-center text_color montserrat_font" style={{ lineHeight: "40px" }}>
+                                                        TECHNOLOGY @ ARMS REACH
+                                                    </h4>
+                                                </div>
+                                                <p className="card_info text-center m-0 text_color roboto_font">
+                                                    The complex and cutting edge technology is always adopted and adapted in our day to day practices with a clear mission to provide world class services
+                                                </p>
+                                            </article>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 card_collapse">
+                                        <div className="card_divider maintain_div mt-4">
+                                            <article className="main_article maintain_div text-center">
+
+                                                <div className="mb-3 maintain_div">
+                                                    <h4 className="h4_title text-center text_color montserrat_font" style={{ lineHeight: "40px" }}>ENVIRONMENT & EQUIPMENT
+                                                    </h4>
+                                                </div>
+                                                <p className="card_info text-center m-0 text_color roboto_font">
+                                                    Utilization of Class B Autoclave Sterilizer is an indication which emphasizes the importance given to hygiene and environmental health. Nevertheless “Cleanliness is close to godliness”
+                                                </p>
+                                            </article>
+                                        </div>
+                                        <div className="card_divider maintain_div mt-4">
+                                            <article className="main_article maintain_div text-center">
+
+                                                <div className="mb-3 maintain_div">
+                                                    <h4 className="h4_title text-center text_color montserrat_font" style={{ lineHeight: "40px" }}>
+                                                        EFFICIENT DOCTORS AND STAFF
+                                                    </h4>
+                                                </div>
+                                                <p className="card_info text-center m-0 text_color roboto_font">
+                                                    The pillars of strength are our experienced dentist, technicians and support staffs who contribute to our success story. Without them neither our patients nor us can adorn the crown
+                                                </p>
+                                            </article>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="dental_services header_margin maintain_div">
+                <div className="dental_services_wrap maintain_div py_80">
+                    <div className="container custom_container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="row">
+                                    <div className="col-lg-4 d-flex">
+                                        <div className="ds_card maintain_div mb-0">
+                                            <div className="ds_card_inner maintain_div">
+                                                <article className="main_article maintain_div text-center">
+                                                    <div className="ds_pic">
+                                                        <img src={baseImagePath('icons/tooth.png')} class="custom_img"
+                                                            alt="Alphabet Internation School" />
+                                                    </div>
+                                                    <div className="hover_change">
+                                                        <img src={baseImagePath('icons/white_tooth.png')} class="custom_img"
+                                                            alt="Alphabet Internation School" />
+                                                    </div>
+                                                    <h4 className="h4_title text-center text_color montserrat_font">VISION
+                                                    </h4>
+                                                    <p className="ds_info text-center m-0 text_color roboto_font">
+                                                        Our Vision is to offer our patients with an amazing dental experience. We also strive to promote lifelong relationship that is built on exceptional patient care, work quality, confidence and trust.
+                                                    </p>
+                                                </article>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 d-flex">
+                                        <div className="ds_card maintain_div mb-0">
+                                            <div className="ds_card_inner maintain_div">
+                                                <article className="main_article maintain_div text-center">
+                                                    <div className="ds_pic">
+                                                        <img src={baseImagePath('icons/tooth.png')} class="custom_img"
+                                                            alt="Alphabet Internation School" />
+                                                    </div>
+                                                    <div className="hover_change">
+                                                        <img src={baseImagePath('icons/white_tooth.png')} class="custom_img"
+                                                            alt="Alphabet Internation School" />
+                                                    </div>
+                                                    <h4 className="h4_title text-center text_color montserrat_font">MISSION
+                                                    </h4>
+                                                    <p className="ds_info text-center m-0 text_color roboto_font">
+                                                        Our Mission is to exceed our clients’ expectations by offering prompt and top quality dental care. We are quite passionate and work hard to instil confidence in our patients ensuring they derive the very best dentistry care we can offer.
+                                                    </p>
+                                                </article>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 d-flex">
+                                        <div className="ds_card maintain_div mb-0">
+                                            <div className="ds_card_inner maintain_div">
+                                                <article className="main_article maintain_div text-center">
+                                                    <div className="ds_pic">
+                                                        <img src={baseImagePath('icons/tooth.png')} class="custom_img"
+                                                            alt="Alphabet Internation School" />
+                                                    </div>
+                                                    <div className="hover_change">
+                                                        <img src={baseImagePath('icons/white_tooth.png')} class="custom_img"
+                                                            alt="Alphabet Internation School" />
+                                                    </div>
+                                                    <h4 className="h4_title text-center text_color montserrat_font">MOTTO
+                                                    </h4>
+                                                    <p className="ds_info text-center m-0 text_color roboto_font">
+                                                        Our Motto is to provide our clients with a caring, positive experience.
+                                                    </p>
+                                                </article>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <section className="parent_speaks maintain_div">
-                <div className="parent_speaks_wrap maintain_div py_80">
+                <div className="parent_speaks_wrap maintain_div">
                     <div className="background_banner">
                         <img src={baseImagePath('thumbnails/theme7.jpg')} class="custom_img"
                             alt="Alphabet Internation School" />
@@ -403,31 +560,6 @@ export default (props) => {
                                         <div className="tc_pic mx-5">
                                             <i class="fa fa-home icon_style" aria-hidden="true"></i>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="social_media maintain_div">
-                <div className="social_media_wrap maintain_div">
-                    <div className="background_banner">
-                        <img src={baseImagePath('thumbnails/theme8.jpg')} class="custom_img"
-                            alt="Alphabet Internation School" />
-                    </div>
-                    <div className="container custom_container">
-                        <div className="ps_main_wrapper ">
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="left_content maintain_div py-5">
-                                        <article className="main_article maintain_div">
-                                            <h4 className="h4_title text-left text_color montserrat_font margin_B20 secondaryColor">SOCIAL MEDIA</h4>
-                                            <h3 className="ds_info text-left text_color montserrat_font">Connect With Us</h3>
-                                            <progress id="file" value="25" max="100" style={{ width: "100%", height: "3px" }}>25%</progress>
-                                            <p className="oc_info text-left text_color roboto_font w-100">
-                                                Strong connection is the key to global success. Travel through our social media channels to know more about our improvisations, treatments, milestones, contended staffs and satisfied clients                                            </p>
-                                        </article>
                                     </div>
                                 </div>
                             </div>
