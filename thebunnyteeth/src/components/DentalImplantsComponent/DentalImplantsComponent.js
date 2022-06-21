6241778604fa0715ff7023e2073a1d5eaebee201
sugumar <sugumar@bitcot.com>
2022-06-21 18:17:27 +0530

    menu components adding

--- thebunnyteeth/src/components/DentalImplantsComponent/DentalImplantsComponent.js
+++ thebunnyteeth/src/components/DentalImplantsComponent/DentalImplantsComponent.js
@@ -138,21 +138,62 @@ export default (props) => {
                                                     </article>
                                                     <div className="maintain_div">
                                                         <ul className="p-1 pt-0">
-                                                            <li className="oc_info text-left text_color roboto_font w-100" style={{listStyleType: "none"}}>
-                                                                <a href="">
-                                                                    <span><i class="fa fa-home icon_style" aria-hidden="true"></i></span>
+                                                            <li className="oc_info text-left text_color roboto_font w-100" style={{ listStyleType: "none" }}>
+                                                                <a href="" className="sidemenu_list roboto_font primary_color">
+                                                                    <span><i class="fa fa-home icon_style pe-2" aria-hidden="true"></i></span>
                                                                     72/60, Bazullah Rd, T.Nagar, TN - 600 017
                                                                 </a>
                                                             </li>
-                                                            <li className="oc_info text-left text_color roboto_font w-100">Dental sealants: We spread this translucent material on your child’s teeth to prevent cavity formation and further spreading.
+                                                            <li className="oc_info text-left text_color roboto_font w-100" style={{ listStyleType: "none" }}>
+                                                                <a href="" className="sidemenu_list roboto_font primary_color">
+                                                                    <span><i class="fa fa-home icon_style pe-2" aria-hidden="true"></i></span>
+                                                                    +(91) 98400 31819
+                                                                </a>
                                                             </li>
-                                                            <li className="oc_info text-left text_color roboto_font w-100">Mouth Guards: We recommend children to wear mouth guards. We create them at our clinic to suit your child’s unique mouth shape.
+                                                            <li className="oc_info text-left text_color roboto_font w-100" style={{ listStyleType: "none" }}>
+                                                                <a href="" className="sidemenu_list roboto_font primary_color">
+                                                                    <span><i class="fa fa-home icon_style pe-2" aria-hidden="true"></i></span>
+                                                                    +(91) 44 42121819
+                                                                </a>
                                                             </li>
-                                                            <li className="oc_info text-left text_color roboto_font w-100">
-                                                                Space Maintainers: We offer this fully customized, specialised dental appliance to suit your child’s mouth.
+                                                            <li className="oc_info text-left text_color roboto_font w-100" style={{ listStyleType: "none" }}>
+                                                                <a href="" className="sidemenu_list roboto_font primary_color">
+                                                                    <span><i class="fa fa-home icon_style pe-2" aria-hidden="true"></i></span>
+                                                                    thebunnyteeth@gmail.com
+                                                                </a>
                                                             </li>
                                                         </ul>
                                                     </div>
+                                                </div>
+                                            </div>
+                                            <div className="clinic_info_wrap maintain_div pt-3">
+                                                <div className="left_content maintain_div">
+                                                    <article className="main_article maintain_div">
+                                                        <h3 className="ds_info text-left text_color montserrat_font" style={{ letterSpacing: "1px" }}>Clinic Hours</h3>
+                                                        <progress id="file" value="25" max="100" style={{ width: "100%", height: "3px" }}>25%</progress>
+                                                    </article>
+                                                    <div className="maintain_div">
+                                                        <ul className="p-1 pt-0">
+                                                            <li className="oc_info text-left text_color roboto_font w-100" style={{ listStyleType: "none" }}>
+                                                                <a href="" className="sidemenu_list roboto_font primary_color">
+                                                                    <span><i class="fa fa-home icon_style pe-2" aria-hidden="true"></i></span>
+                                                                    Monday to Saturday
+                                                                </a>
+                                                            </li>
+                                                            <li className="oc_info text-left text_color roboto_font w-100" style={{ listStyleType: "none" }}>
+                                                                <a href="" className="sidemenu_list roboto_font primary_color">
+                                                                    <span><i class="fa fa-home icon_style pe-2" aria-hidden="true"></i></span>
+                                                                    10:00 AM to 8:00 PM
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="oc_card maintain_div">
+                                                <div className="video_data">
+                                                    <iframe width="100%" height="200" src="https://www.youtube.com/embed/K4qvMAxWWfQ?feature=oembed&rel=0&mute=0&loop=0&controls=1&autoplay=1">
+                                                    </iframe>
                                                 </div>
                                             </div>
                                         </div>
@@ -163,7 +204,124 @@ export default (props) => {
                     </div>
                 </div>
             </section>
-
+            <section className="quote_sec maintain_div padding_50">
+                <div className="app_banner_wrap maintain_div">
+                    <div className="container custom_container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="quote_content maintain_div text-center pb-3">
+                                    <h4 className="h4_title montserrat_font secontary_color">We Care for your teeth!
+                                    </h4>
+                                    <div className="maintain_div mt-4">
+                                        <a href="#menu" class="primary_btn roboto_font fw-bold me-5">Discover More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="dental_services header_margin why_choose">
+                <div className="dental_services_wrap maintain_div py_80 primary_color_bg position-relative">
+                    <div className="container custom_container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="row">
+                                    <div className="col-lg-8">
+                                        <header className="maintain_div why_choose_title text-center">
+                                            <h3 className="h3_title montserrat_font m-0 text-white montserrat_font">Why Choose Us
+                                            </h3>
+                                        </header>
+                                        <div className="row">
+                                            <div className="col-lg-6">
+                                                <div className="media_object py-5">
+                                                    <div class="d-flex">
+                                                        <div class="flex-shrink-0">
+                                                            <div className="ds_pic">
+                                                                <img src={baseImagePath('icons/white_tooth.png')} class="custom_img"
+                                                                    alt="Alphabet Internation School" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="flex-grow-1 ms-3">
+                                                            <h5 className="h5_title text-white text_color montserrat_font mb-3 w-75">TECHNOLOGY @ ARMS REACH</h5>
+                                                            <p className="ds_info text-white m-0 text_color roboto_font w-75">
+                                                                The complex and cutting edge technology is always adopted and adapted in our day to day practices with a clear mission to provide world class services                                                             </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-6">
+                                                <div className="media_object py-5">
+                                                    <div class="d-flex">
+                                                        <div class="flex-shrink-0">
+                                                            <div className="ds_pic">
+                                                                <img src={baseImagePath('icons/white_tooth.png')} class="custom_img"
+                                                                    alt="Alphabet Internation School" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="flex-grow-1 ms-3">
+                                                            <h5 className="h5_title text-white text_color montserrat_font mb-3 w-75">EFFICIENT team</h5>
+                                                            <p className="ds_info text-white m-0 text_color roboto_font w-75">
+                                                                The pillars of strength are our experienced dentist, technicians and support staffs who contribute to our success story. Without them neither our patients nor us can adorn the crown
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-6">
+                                                <div className="media_object py-5 pb-0">
+                                                    <div class="d-flex">
+                                                        <div class="flex-shrink-0">
+                                                            <div className="ds_pic">
+                                                                <img src={baseImagePath('icons/white_tooth.png')} class="custom_img"
+                                                                    alt="Alphabet Internation School" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="flex-grow-1 ms-3">
+                                                            <h5 className="h5_title text-white text_color montserrat_font mb-3 w-75">REVIEW MECHANISM</h5>
+                                                            <p className="ds_info text-white m-0 text_color roboto_font w-75">
+                                                                Our close call on each and every patient, before and after their treatment, gives them immense confidence that they are being taken care in the right way
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-6">
+                                                <div className="media_object py-5 pb-0">
+                                                    <div class="d-flex">
+                                                        <div class="flex-shrink-0">
+                                                            <div className="ds_pic">
+                                                                <img src={baseImagePath('icons/white_tooth.png')} class="custom_img"
+                                                                    alt="Alphabet Internation School" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="flex-grow-1 ms-3">
+                                                            <h5 className="h5_title text-white text_color montserrat_font mb-3 w-75">ENVIRONMENT & EQUIPMENT</h5>
+                                                            <p className="ds_info text-white m-0 text_color roboto_font w-75">
+                                                                Utilization of Class B Autoclave Sterilizer is an indication which emphasizes the importance given to hygiene and environmental health. Nevertheless “Cleanliness is close to godliness”
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 d-flex">
+                                        <div className="why_background_image maintain_div h-100">
+                                            <div className="why_background_image_wrap maintain_div h-100">
+                                                <div className="inner_wrap w-100 h-100">
+                                                    <img src={baseImagePath('thumbnails/hero.jpg')} class="custom_img"
+                                                        alt="Alphabet Internation School" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
