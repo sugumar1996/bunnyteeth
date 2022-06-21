68931985abb60a54369e883eb537996e36d2f730
sugumar <sugumar@bitcot.com>
2022-06-21 20:01:15 +0530

    menu components adding

--- thebunnyteeth/src/router/NonLoggedInRouter.js
+++ thebunnyteeth/src/router/NonLoggedInRouter.js
@@ -1,9 +1,16 @@
 import AboutContainer from "containers/AboutContainer/AboutContainer";
+import BridgesContainer from "containers/BridgesContainer/BridgesContainer";
+import ClearAligenersContainer from "containers/ClearAligenersContainer/ClearAligenersContainer";
 import DentalImplantsContainer from "containers/DentalImplantsContainer/DentalImplantsContainer";
+import DenturesContainer from "containers/DenturesContainer/DenturesContainer";
 import GeneralDentistryContainer from "containers/GeneralDentistryContainer/GeneralDentistryContainer";
 import HomeContainer from "containers/HomeContainer/HomeContainer";
+import KidsDensitryContainer from "containers/KidsDensitryContainer/KidsDensitryContainer";
+import OrthodonticContainer from "containers/OrthodonticContainer/OrthodonticContainer";
+import SedationDensitryContainer from "containers/SedationDensitryContainer/SedationDensitryContainer";
 import SmileMakeOverContainer from "containers/SmileMakeOverContainer/SmileMakeOverContainer";
 import TreatmentsContainer from "containers/TreatmentsContainer/TreatmentsContainer";
+import WisdowToothContainer from "containers/WisdowToothContainer/WisdowToothContainer";
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { routes } from "../utility/constants/constants";
@@ -17,7 +24,15 @@ export default () => {
       <Route exact path={routes.TREATMENTS} component={TreatmentsContainer} />
       <Route exact path={routes.GENERAL_DENTISTRY} component={GeneralDentistryContainer} />
       <Route exact path={routes.SMILE_MAKE_OVER} component={SmileMakeOverContainer} />
-      <Route exact path={routes.DENTAL_IMPLANTS} component={DentalImplantsContainer} />
+      <Route exact path={routes.KIDS_DENTISTRY} component={KidsDensitryContainer} />
+      <Route exact path={routes.SEDATION_DENTISTY} component={SedationDensitryContainer} />
+      <Route exact path={routes.DENTURES} component={DenturesContainer} />
+      <Route exact path={routes.BRIDHES_CROWNS} component={BridgesContainer} />
+      <Route exact path={routes.CLEAR_ALIGNERS_INVISIBLE_BRACES} component={ClearAligenersContainer} />
+      <Route exact path={routes.ORTHODONTIC_TREATMENT} component={OrthodonticContainer} />
+      <Route exact path={routes.WISDOW_TOOTH_REMOVAL} component={WisdowToothContainer} />
+      <Route exact path={routes.ROOT_CANAL} component={KidsDensitryContainer} />
+
       <Route exact path="*" component={() => <Redirect to={routes.HOME_BLOG} />} />
     </Switch>
   );
